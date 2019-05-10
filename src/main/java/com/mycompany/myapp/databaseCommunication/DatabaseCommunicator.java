5d074cd4856643e3044066a9a3ec2cdf1f3afb9e
mandzeete <aivar.loopalu@gmail.com>
2019-05-10 15:07:16 +0300

    Rest API saab kätte headeri.

--- src/main/java/com/mycompany/myapp/databaseCommunication/DatabaseCommunicator.java
+++ src/main/java/com/mycompany/myapp/databaseCommunication/DatabaseCommunicator.java
@@ -1,18 +1,26 @@
 package com.mycompany.myapp.databaseCommunication;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mycompany.myapp.domain.Experiment;
 import com.mycompany.myapp.web.rest.AnalytesOfInterestResource;
+import com.mycompany.myapp.web.rest.errors.BadRequestAlertException;
+import com.mycompany.myapp.web.rest.util.HeaderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Base64;
 
 @RestController
 public class DatabaseCommunicator {
@@ -32,6 +40,14 @@ public class DatabaseCommunicator {
     @GetMapping("/getMatrixes")
     public String getMatrixes() {
         return getter("matrixes");
+    }
+
+    @GetMapping(path = "/postMethod")
+    public void postMethod(@RequestHeader("Data") String string) {
+        byte[] decodedBytes = Base64.getDecoder().decode(string);
+        String decodedString = new String(decodedBytes);
+        System.out.println("qweqweqe");
+        System.out.println(decodedString);
     }
 
     @GetMapping("/getUsers")
---
+++ src/main/java/com/mycompany/myapp/databaseCommunication/Method.java
@@ -0,0 +1,216 @@
+package com.mycompany.myapp.databaseCommunication;
+
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Method {
+    private String nameOfTest;
+    private String nameOfUser;
+    private String userClass;
+    private ObservableList<Analyte> analytes;
+    //private HashMap<String,Integer> analyteConcentrations = new HashMap<>();
+    private String analyteUnit;
+    private String matrix;
+    private ObservableList<Analyte> bge;
+    //private HashMap<String, Integer> bgeConcentrations = new HashMap<>();
+    private String bgeUnit;
+    private String capillary;
+    private String capillaryTotalLength;
+    private String capillaryEffectiveLength;
+    private String injectionMethod;
+    private String injectionChoice;
+    private String injectionChoiceValue;
+    private String injectionChoiceUnit;
+    private String injectionTime;
+    private String current;
+    private String frequency;
+    private String description;
+    private String hvValue;
+    private String testTime;
+
+    public String getNameOfUser() {
+        return nameOfUser;
+    }
+
+    public void setNameOfUser(String nameOfUser) {
+        this.nameOfUser = nameOfUser;
+    }
+
+    public ObservableList<Analyte> getAnalytes() {
+        return analytes;
+    }
+
+    public void setAnalytes(ObservableList<Analyte> analytes) {
+        this.analytes = analytes;
+    }
+
+//    public HashMap<String, Integer> getAnalyteConcentrations() {
+//        return analyteConcentrations;
+//    }
+//
+//    public void setAnalyteConcentrations(HashMap<String, Integer> analyteConcentrations) {
+//        this.analyteConcentrations = analyteConcentrations;
+//    }
+
+    public String getAnalyteUnit() {
+        return analyteUnit;
+    }
+
+    public void setAnalyteUnit(String analyteUnit) {
+        this.analyteUnit = analyteUnit;
+    }
+
+    public String getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(String matrix) {
+        this.matrix = matrix;
+    }
+
+    public ObservableList<Analyte> getBge() {
+        return bge;
+    }
+
+    public void setBge(ObservableList<Analyte> bge) {
+        this.bge = bge;
+    }
+
+//    public HashMap<String, Integer> getBgeConcentrations() {
+//        return bgeConcentrations;
+//    }
+//
+//    public void setBgeConcentrations(HashMap<String, Integer> bgeConcentrations) {
+//        this.bgeConcentrations = bgeConcentrations;
+//    }
+
+    public String getBgeUnit() {
+        return bgeUnit;
+    }
+
+    public void setBgeUnit(String bgeUnit) {
+        this.bgeUnit = bgeUnit;
+    }
+
+    public String getCapillary() {
+        return capillary;
+    }
+
+    public void setCapillary(String capillary) {
+        this.capillary = capillary;
+    }
+
+    public String getCapillaryTotalLength() {
+        return capillaryTotalLength;
+    }
+
+    public void setCapillaryTotalLength(String capillaryTotalLength) {
+        this.capillaryTotalLength = capillaryTotalLength;
+    }
+
+    public String getCapillaryEffectiveLength() {
+        return capillaryEffectiveLength;
+    }
+
+    public void setCapillaryEffectiveLength(String capillaryEffectiveLength) {
+        this.capillaryEffectiveLength = capillaryEffectiveLength;
+    }
+
+    public String getInjectionMethod() {
+        return injectionMethod;
+    }
+
+    public void setInjectionMethod(String injectionMethod) {
+        this.injectionMethod = injectionMethod;
+    }
+
+    public String getInjectionChoice() {
+        return injectionChoice;
+    }
+
+    public void setInjectionChoice(String injectionChoice) {
+        this.injectionChoice = injectionChoice;
+    }
+
+    public String getInjectionChoiceValue() {
+        return injectionChoiceValue;
+    }
+
+    public void setInjectionChoiceValue(String injectionChoiceValue) {
+        this.injectionChoiceValue = injectionChoiceValue;
+    }
+
+    public String getInjectionChoiceUnit() {
+        return injectionChoiceUnit;
+    }
+
+    public void setInjectionChoiceUnit(String injectionChoiceUnit) {
+        this.injectionChoiceUnit = injectionChoiceUnit;
+    }
+
+    public String getInjectionTime() {
+        return injectionTime;
+    }
+
+    public void setInjectionTime(String injectionTime) {
+        this.injectionTime = injectionTime;
+    }
+
+    public String getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(String current) {
+        this.current = current;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHvValue() {
+        return hvValue;
+    }
+
+    public void setHvValue(String hvValue) {
+        this.hvValue = hvValue;
+    }
+
+    public String getNameOfTest() {
+        return nameOfTest;
+    }
+
+    public void setNameOfTest(String nameOfTest) {
+        this.nameOfTest = nameOfTest;
+    }
+
+    public String getUserClass() {
+        return userClass;
+    }
+
+    public void setUserClass(String userClass) {
+        this.userClass = userClass;
+    }
+
+    public String getTestTime() {
+        return testTime;
+    }
+
+    public void setTestTime(String testTime) {
+        this.testTime = testTime;
+    }
+}
